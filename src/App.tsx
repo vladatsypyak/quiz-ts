e6417ec101858759bfc736ea94c615ee53d567aa
Vladyslava <vladatsyupyak@gmail.com>
2024-08-05 22:50:06 +0300

    feat: add auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from "./components/auth/Login";
+import SignUp from "./components/auth/SignUp";
+import { Routes, Route } from 'react-router-dom';
+import Main from "./components/Main/Main";
+import Header from "./components/Header/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Routes>
+                <Route path={"/signup"} element={<SignUp/>}/>
+                <Route path={"/signin"} element={<Login/>}/>
+                <Route path={"/"} element={<Main/>}/>
+
+            </Routes>
+        </div>
+    );
+
 }
 
 export default App;
