d923657c761e58cd36de933cf226cf85b9653680
Vladyslava <vladatsyupyak@gmail.com>
2024-08-08 14:13:48 +0300

    style: add image to main page + animation

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,17 @@
 import React, {useState} from "react";
 import s from "./main.module.scss"
-
+import logo from "../../images/big_logo.png"
 const Main = () => {
     return (
         <div className={s.main}>
-            <button>грати</button>
-            <button>створити квіз</button>
+            <div className={s.image_wrap}>
+                <img className={s.image} src={logo} alt="logo"/>
+            </div>
+            <div className={s.flex_wrap}>
+                <button className={`${s.btn} ${s.btn_play}`}>грати</button>
+                <button className={`${s.btn} ${s.btn_create}`}>створити квіз</button>
+            </div>
+
         </div>
     )
 }
