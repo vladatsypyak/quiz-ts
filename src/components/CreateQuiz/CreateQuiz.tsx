7263508809109e5570b2caa841a1d6a8005fc0af
Vladyslava <vladatsyupyak@gmail.com>
2024-08-25 16:55:31 +0300

    feat: store quiz data in db

--- src/components/CreateQuiz/CreateQuiz.tsx
+++ src/components/CreateQuiz/CreateQuiz.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import s from "./createQuiz.module.scss"
 import Option from "./Option/Option";
-import { collection, addDoc } from "firebase/firestore";
+import {collection, addDoc} from "firebase/firestore";
 import {db} from '../../firebase';
 
 
@@ -11,7 +11,7 @@ type Option = {
 }
 
 type HandleOptionChangeParams = {
-    index :number;
+    index: number;
     optionIndex: number;
     field: keyof Option;
     value: string
@@ -21,11 +21,13 @@ type HandleOptionChangeParams = {
 const CreateQuiz = () => {
     const [quizData, setQuizData] = useState([{
         index: 0,
-        question : "",
-        options: [{text: "", image: ""}]}])
+        question: "",
+        options: [{text: "", image: ""}]
+    }])
+    const [title, setTitle] = useState("")
 
 
-    const handleOptionChange = ({index, optionIndex, field, value}: HandleOptionChangeParams ) =>{
+    const handleOptionChange = ({index, optionIndex, field, value}: HandleOptionChangeParams) => {
         const newQuizData = [...quizData];
         const newOptions = [...newQuizData[index].options];
         newOptions[optionIndex] = {
@@ -35,11 +37,26 @@ const CreateQuiz = () => {
         newQuizData[index].options = newOptions;
         setQuizData(newQuizData);
     }
-    const testDb = async (e: any) => {
+
+    function handleQuestionChange(e: any, index: number) {
+        const newQuizData = [...quizData];
+        newQuizData[index].question = e.target.value
+        setQuizData(newQuizData)
+        console.log(quizData)
+    }
+
+    function handleTitleChange(e: any) {
+        setTitle(e.target.value)
+    }
+
+    const storeQuizData = async (e: any) => {
         e.preventDefault();
         try {
-            const docRef = await addDoc(collection(db, "new"), {
-                name: "test",
+            const docRef = await addDoc(collection(db, "quiz"), {
+                quizData: {
+                    ...quizData,
+                    title: title
+                },
             });
             console.log("Document written with ID: ", docRef.id);
         } catch (e) {
@@ -51,24 +68,24 @@ const CreateQuiz = () => {
         <form className={s.container}>
             <div className={s.title_wrap}>
                 <p className={s.title}>Назва</p>
-                <input placeholder={"Назва квізу"} type="text"/>
+                <input onChange={handleTitleChange} placeholder={"Назва квізу"} type="text"/>
             </div>
             <div className={s.question_section}>
                 <p className={s.number}>1</p>
                 <div className={s.question_wrap}>
-                    <input className={s.question} type="text"/>
+                    <input onChange={(e) => handleQuestionChange(e, 0)} className={s.question} type="text"/>
                     <div className={s.options_wrap}>
                         <div className={s.option}>
                             <Option handleOptionChange={handleOptionChange} optionIndex={0} index={0}/>
                         </div>
                         <div className={s.option}>
-                            <Option  handleOptionChange={handleOptionChange} optionIndex={1} index={0}/>
+                            <Option handleOptionChange={handleOptionChange} optionIndex={1} index={0}/>
                         </div>
                         <div className={s.option}>
                             <Option handleOptionChange={handleOptionChange} optionIndex={2} index={0}/>
                         </div>
                         <div className={s.option}>
-                            <Option  handleOptionChange={handleOptionChange} optionIndex={3} index={0}/>
+                            <Option handleOptionChange={handleOptionChange} optionIndex={3} index={0}/>
                         </div>
                     </div>
                 </div>
@@ -77,7 +94,9 @@ const CreateQuiz = () => {
                     <button>d</button>
                 </div>
             </div>
-            <button >+</button>
+            <button>+</button>
+            <button onClick={storeQuizData}>save</button>
+
         </form>
     )
 }
