1fc4df95a5ae082efd2449a32b508f5671b0085e
Vladyslava <vladatsyupyak@gmail.com>
2024-08-25 22:54:46 +0300

    feat: display array og questions and add new one

--- src/components/CreateQuiz/CreateQuiz.tsx
+++ src/components/CreateQuiz/CreateQuiz.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {ChangeEvent, FormEvent, useState} from "react";
 import s from "./createQuiz.module.scss"
 import Option from "./Option/Option";
 import {collection, addDoc} from "firebase/firestore";
@@ -6,8 +6,8 @@ import {db} from '../../firebase';
 
 
 type Option = {
-    text: "sting",
-    image: "string"
+    text: string,
+    image: string
 }
 
 type HandleOptionChangeParams = {
@@ -17,14 +17,23 @@ type HandleOptionChangeParams = {
     value: string
 }
 
+type Question = {
+    index: number,
+    question: string,
+    options: Option[]
+}
 
-const CreateQuiz = () => {
-    const [quizData, setQuizData] = useState([{
+const quizDataInitialSate = [
+    {
         index: 0,
         question: "",
-        options: [{text: "", image: ""}]
-    }])
-    const [title, setTitle] = useState("")
+        options: [{text: "", image: ""}, {text: "", image: ""}, {text: "", image: ""}, {text: "", image: ""}]
+    }
+]
+
+const CreateQuiz = () => {
+    const [quizData, setQuizData] = useState<Question[]>(quizDataInitialSate)
+    const [title, setTitle] = useState<string>("")
 
 
     const handleOptionChange = ({index, optionIndex, field, value}: HandleOptionChangeParams) => {
@@ -38,18 +47,20 @@ const CreateQuiz = () => {
         setQuizData(newQuizData);
     }
 
-    function handleQuestionChange(e: any, index: number) {
+    function handleQuestionChange(e: ChangeEvent<HTMLInputElement>, index: number) {
         const newQuizData = [...quizData];
         newQuizData[index].question = e.target.value
         setQuizData(newQuizData)
         console.log(quizData)
     }
 
-    function handleTitleChange(e: any) {
+    console.log(quizData)
+
+    function handleTitleChange(e: ChangeEvent<HTMLInputElement>) {
         setTitle(e.target.value)
     }
 
-    const storeQuizData = async (e: any) => {
+    const storeQuizData = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         try {
             const docRef = await addDoc(collection(db, "quiz"), {
@@ -63,39 +74,50 @@ const CreateQuiz = () => {
             console.error("Error adding document: ", e);
         }
     }
+    const handleAddQuestionBtn = (e: any) => {
+        e.preventDefault()
+        const newQuizData = [...quizData, {
+            index: 0,
+            question: "",
+            options: [{text: "", image: ""}, {text: "", image: ""}, {text: "", image: ""}, {text: "", image: ""}]
+        }]
+        setQuizData(newQuizData)
+    }
 
     return (
-        <form className={s.container}>
+        <form onSubmit={storeQuizData} className={s.container}>
+
             <div className={s.title_wrap}>
                 <p className={s.title}>Назва</p>
                 <input onChange={handleTitleChange} placeholder={"Назва квізу"} type="text"/>
             </div>
-            <div className={s.question_section}>
-                <p className={s.number}>1</p>
-                <div className={s.question_wrap}>
-                    <input onChange={(e) => handleQuestionChange(e, 0)} className={s.question} type="text"/>
-                    <div className={s.options_wrap}>
-                        <div className={s.option}>
-                            <Option handleOptionChange={handleOptionChange} optionIndex={0} index={0}/>
+
+            {quizData.map(el => {
+                return <div className={s.question_section}>
+                    <p className={s.number}>{el.index + 1}</p>
+                    <div className={s.question_wrap}>
+                        <input onChange={(e) => handleQuestionChange(e, el.index)} className={s.question} type="text"/>
+                        <div className={s.options_wrap}>
+                            {el.options.map(option => {
+                                return <div className={s.option}>
+                                    <Option handleOptionChange={handleOptionChange}
+                                            optionIndex={el.options.indexOf(option)} index={el.index}/>
+                                </div>
+                            })}
                         </div>
-                        <div className={s.option}>
-                            <Option handleOptionChange={handleOptionChange} optionIndex={1} index={0}/>
-                        </div>
-                        <div className={s.option}>
-                            <Option handleOptionChange={handleOptionChange} optionIndex={2} index={0}/>
-                        </div>
-                        <div className={s.option}>
-                            <Option handleOptionChange={handleOptionChange} optionIndex={3} index={0}/>
-                        </div>
+
+                    </div>
+                    <div className={s.question_buttons}>
+                        <button>c</button>
+                        <button>d</button>
                     </div>
                 </div>
-                <div className={s.question_buttons}>
-                    <button>c</button>
-                    <button>d</button>
-                </div>
-            </div>
-            <button>+</button>
-            <button onClick={storeQuizData}>save</button>
+            })}
+
+            <button onClick={handleAddQuestionBtn}>+</button>
+
+
+            <button type={"submit"}>save</button>
 
         </form>
     )
