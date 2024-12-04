8e071a0117d5a7d8a43eb158955cfd9c675e97bf
Vladyslava <vladatsyupyak@gmail.com>
2024-12-04 15:35:45 +0200

    feat: log out button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,24 +3,32 @@ import {Link} from "react-router-dom";
 import s from "./header.module.scss"
 import {useSelector} from "react-redux";
 import {RootState} from "../../redux/store";
+import { getAuth, signOut } from "firebase/auth";
 
 function Header() {
     const user = useSelector((store: RootState) => store.userSlice.user)
+    const auth = getAuth();
 
-
+    const onLogoutClick = () => {
+        signOut(auth).then(() => {
+            console.log("Signed out")
+        }).catch((error) => {
+            console.log(error)
+        });
+    }
     return (
-       <header className={s.header}>
-           <div className={`wrapper ${s.container}`}>
-               <p>лого</p>
+        <header className={s.header}>
+            <div className={`wrapper ${s.container}`}>
+                <p>лого</p>
 
-             <div className={s.icons}>
-                 {user ? <Link to={"/home"}>home</Link>  : null }
+                <div className={s.icons}>
+                    {user ? <Link to={"/home"}>home</Link> : null}
 
-                 {user ? <Link to={"/signout"}>вийти</Link>  : <Link to={"/signup"}>Ввійти</Link>  }
-             </div>
+                    {user ? <button onClick={onLogoutClick}>вийти</button> : <Link to={"/signup"}>Ввійти</Link>}
+                </div>
 
-           </div>
-       </header>
+            </div>
+        </header>
     );
 }
 
