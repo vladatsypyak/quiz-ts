8249e893147470849e4bc8bb4ff08a37075deb1a
Vladyslava <vladatsyupyak@gmail.com>
2024-12-19 15:12:29 +0200

    feat: update quiz

--- src/components/Home/UserQuizes/FullQuiz.tsx
+++ src/components/Home/UserQuizes/FullQuiz.tsx
@@ -1,13 +1,15 @@
-import React, {useEffect, useState} from "react";
+import React, {FormEvent, useEffect, useState} from "react";
 import s from "./userQuizes.module.scss"
 import {Link, useNavigate} from "react-router-dom";
 import {useSelector} from "react-redux";
 import {RootState} from "../../../redux/store";
 import {fetchQuizById, QuizType} from "../../../redux/slices/quizzesSlice";
 import { useParams } from 'react-router-dom';
-import { DocumentData } from "firebase/firestore";
+import {updateDoc, collection, doc, DocumentData} from "firebase/firestore";
 import QuizTemplate from "../../shared/QuizTemplate/QuizTemplate";
 import {log} from "util";
+import {db} from "../../../firebase";
+
 
 
 
@@ -34,10 +36,22 @@ const FullQuiz = () => {
         fetchData();
     }, [id]);
 
+    const onSubmit = async (e: FormEvent<HTMLFormElement>, quizData: any, title: any) =>{
+        e.preventDefault();
+
+        const docRef = doc(collection(db, 'quiz'), id);
+        const updated = await updateDoc(docRef,{
+            title:title,
+            questions : [...quizData],
+        } )
+        console.log("successfully updated")
+    }
+
+
     return (
-        <div className={s.quiz_wrapper}>
+        <div className={""}>
             {
-              quiz ?  <QuizTemplate initialQuizData={quiz?.questions} initialTitle={quiz?.title} storeQuizData={()=> console.log("hi")}/> : null
+              quiz ?  <QuizTemplate initialQuizData={quiz?.questions} initialTitle={quiz?.title} storeQuizData={onSubmit}/> : null
 
             }
 
